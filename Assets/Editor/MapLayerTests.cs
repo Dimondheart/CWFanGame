60587805a259bae3862e8f788c8347a39f8d6056
Bryan Bettis <bryancbettis@gmail.com>
2017-11-01 23:04:18 -0700

    B1|Added Map Layers
    - Added MapLayer
    - Added MapLayer unit tests
    - Added terrain tile prefabs for future testing/prototype use

---
+++ Assets/Editor/MapLayerTests.cs
@@ -0,0 +1,265 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using TechnoWolf.CWFanGame;
+
+public class MapLayerTests
+{
+	/**<summary>Get a new non-mock MapLayer for testing.</summary>*/
+	private MapLayer GetTestMapLayer()
+	{
+		return new GameObject().AddComponent<MapLayer>();
+	}
+
+	[Test]
+	public void MapLayer_UninitializedWidth()
+	{
+		MapLayer mapLayer = GetTestMapLayer();
+
+		Assert.AreEqual(-1, mapLayer.Width);
+	}
+
+	[Test]
+	public void MapLayer_UninitializedHeight()
+	{
+		MapLayer mapLayer = GetTestMapLayer();
+
+		Assert.AreEqual(-1, mapLayer.Height);
+	}
+
+	[Test]
+	public void MapLayer_ResizeAndClear_Width()
+	{
+		int width = 12;
+		int height = 15;
+		MapLayer mapLayer = GetTestMapLayer();
+
+		mapLayer.ResizeAndClear(width, height);
+
+		Assert.AreEqual(width, mapLayer.Width);
+	}
+
+	[Test]
+	public void MapLayer_ResizeAndClear_Height()
+	{
+		int width = 12;
+		int height = 15;
+		MapLayer mapLayer = GetTestMapLayer();
+
+		mapLayer.ResizeAndClear(width, height);
+
+		Assert.AreEqual(height, mapLayer.Height);
+	}
+
+	[Test]
+	public void MapLayer_ResizeAndClear_Depth()
+	{
+		int width = 12;
+		int height = 15;
+		float depth = 4.0f;
+		MapLayer mapLayer = GetTestMapLayer();
+
+		mapLayer.ResizeAndClear(width, height, depth);
+
+		foreach (float d in mapLayer.depth)
+		{
+			Assert.AreEqual(depth, d);
+		}
+	}
+
+	[Test]
+	public void MapLayer_ResizeAndClear_DeltaDepth()
+	{
+		int width = 12;
+		int height = 15;
+		float depth = 4.0f;
+		MapLayer mapLayer = GetTestMapLayer();
+
+		mapLayer.ResizeAndClear(width, height, depth);
+
+		foreach (float d in mapLayer.deltaDepth)
+		{
+			Assert.AreEqual(0.0f, d);
+		}
+	}
+
+	[Test]
+	public void MapLayer_Clear_Width()
+	{
+		int width = 12;
+		int height = 15;
+		MapLayer mapLayer = GetTestMapLayer();
+		mapLayer.ResizeAndClear(width, height);
+
+		mapLayer.Clear();
+
+		Assert.AreEqual(width, mapLayer.Width);
+	}
+
+	[Test]
+	public void MapLayer_Clear_Height()
+	{
+		int width = 12;
+		int height = 15;
+		MapLayer mapLayer = GetTestMapLayer();
+		mapLayer.ResizeAndClear(width, height);
+
+		mapLayer.Clear();
+
+		Assert.AreEqual(height, mapLayer.Height);
+	}
+
+	[Test]
+	public void MapLayer_Clear_Depth()
+	{
+		int width = 12;
+		int height = 15;
+		float depth = 4.0f;
+		MapLayer mapLayer = GetTestMapLayer();
+		mapLayer.ResizeAndClear(width, height);
+
+		mapLayer.Clear(depth);
+
+		foreach (float d in mapLayer.depth)
+		{
+			Assert.AreEqual(depth, d);
+		}
+	}
+
+	[Test]
+	public void MapLayer_Clear_DeltaDepth()
+	{
+		int width = 12;
+		int height = 15;
+		float depth = 4.0f;
+		MapLayer mapLayer = GetTestMapLayer();
+		mapLayer.ResizeAndClear(width, height);
+
+		mapLayer.Clear(depth);
+
+		foreach (float d in mapLayer.deltaDepth)
+		{
+			Assert.AreEqual(0.0f, d);
+		}
+	}
+
+	[Test]
+	public void MapLayer_ApplyDeltaDepths_Width()
+	{
+		int width = 12;
+		int height = 15;
+		MapLayer mapLayer = GetTestMapLayer();
+		mapLayer.ResizeAndClear(width, height);
+		mapLayer.deltaDepth[width - 1, height - 1] = 1.0f;
+
+		mapLayer.ApplyDeltaDepths();
+
+		Assert.AreEqual(width, mapLayer.Width);
+	}
+
+	[Test]
+	public void MapLayer_ApplyDeltaDepths_Height()
+	{
+		int width = 12;
+		int height = 15;
+		MapLayer mapLayer = GetTestMapLayer();
+		mapLayer.ResizeAndClear(width, height);
+		mapLayer.deltaDepth[width - 1, height - 1] = 1.0f;
+
+		mapLayer.ApplyDeltaDepths();
+
+		Assert.AreEqual(height, mapLayer.Height);
+	}
+
+	[Test]
+	public void MapLayer_ApplyDeltaDepths_Depth()
+	{
+		int width = 12;
+		int height = 15;
+		MapLayer mapLayer = GetTestMapLayer();
+		mapLayer.ResizeAndClear(width, height);
+
+		float[,] initialDepth = new float[width, height];
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				initialDepth[x, y] = (float)(x + y);
+				mapLayer.depth[x, y] = (float)(x + y);
+			}
+		}
+		float[,] deltaDepth = new float[width, height];
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				deltaDepth[x, y] = (float)(x);
+				mapLayer.deltaDepth[x, y] = (float)(x);
+			}
+		}
+		float[,] expectedDepth = new float[width, height];
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				expectedDepth[x, y] = initialDepth[x, y] + deltaDepth[x, y];
+			}
+		}
+		mapLayer.ApplyDeltaDepths();
+
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				Assert.AreEqual(expectedDepth[x, y], mapLayer.depth[x, y]);
+			}
+		}
+	}
+
+	[Test]
+	public void MapLayer_ApplyDeltaDepths_DeltaDepth()
+	{
+		int width = 12;
+		int height = 15;
+		MapLayer mapLayer = GetTestMapLayer();
+		mapLayer.ResizeAndClear(width, height);
+
+		float[,] initialDepth = new float[width, height];
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				initialDepth[x, y] = (float)(x + y);
+				mapLayer.depth[x, y] = (float)(x + y);
+			}
+		}
+		float[,] deltaDepth = new float[width, height];
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				deltaDepth[x, y] = (float)(x);
+				mapLayer.deltaDepth[x, y] = (float)(x);
+			}
+		}
+		float[,] expectedDepth = new float[width, height];
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				expectedDepth[x, y] = initialDepth[x, y] + deltaDepth[x, y];
+			}
+		}
+		mapLayer.ApplyDeltaDepths();
+
+		for (int x = 0; x < width; x++)
+		{
+			for (int y = 0; y < height; y++)
+			{
+				Assert.AreEqual(0.0f, mapLayer.deltaDepth[x, y]);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TechnoWolf/CWFanGame/MapLayer.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TechnoWolf.CWFanGame
+{
+	/**<summary>Per tile depth for terrain, fluids, and anything else that
+	 * covers the entire map and has a volatile per-tile depth.</summary>
+	 */
+	public class MapLayer : MonoBehaviour
+	{
+		/**<summary>The depth/thickness of this layer for each tile.</summary>*/
+		public float[,] depth { get; private set; }
+		/**<summary>The change in depth of this layer for each tile for the current
+		 * cycle. This is applied and reset at the end of each tile update
+		 * cycle.</summary>
+		 */
+		public float[,] deltaDepth { get; private set; }
+		/**<summary>The width (in tiles) of this layer. Has a value of
+		 * -1 if the layer has not been initialized.</summary>
+		 */
+		public int Width
+		{
+			get
+			{
+				return depth == null ? -1 : depth.GetLength(0);
+			}
+		}
+		/**<summary>The height (in tiles) of this layer. Has a value of
+		 * -1 if the layer has not been initialized.</summary>
+		 */
+		public int Height
+		{
+			get
+			{
+				return depth == null ? -1 : depth.GetLength(1);
+			}
+		}
+
+		public void FixedUpdate()
+		{
+			ApplyDeltaDepths();
+		}
+
+		/**<summary>Clear the layer, setting all tile depths to the same
+		 * value and all depth deltas to 0.</summary>
+		 * <param name="value">The value to set all tile depths to</param>
+		 */
+		public void Clear(float value = 0.0f)
+		{
+			for (int x = 0; x < Width; x++)
+			{
+				for (int y = 0; y < Height; y++)
+				{
+					depth[x, y] = value;
+					deltaDepth[x, y] = 0.0f;
+				}
+			}
+		}
+
+		/**<summary>Resize and clear the layer.</summary>
+		 * <param name="width">New width, in tiles</param>
+		 * <param name="height">New height, in tiles</param>
+		 * <param name="value">Clear depth to this value for each tile</param>
+		 */
+		public void ResizeAndClear(int width, int height, float value = 0.0f)
+		{
+			if (width != Width || height != Height)
+			{
+				depth = new float[width, height];
+				deltaDepth = new float[width, height];
+			}
+			Clear(value);
+		}
+
+		/**<summary>Applies the depth deltas and resets all depth deltas to 0.</summary>*/
+		public void ApplyDeltaDepths()
+		{
+			for (int x = 0; x < Width; x++)
+			{
+				for (int y = 0; y < Height; y++)
+				{
+					if (deltaDepth[x, y] != 0.0f)
+					{
+						depth[x, y] += deltaDepth[x, y];
+					}
+					deltaDepth[x, y] = 0.0f;
+				}
+			}
+		}
+	}
+}
